b959d3b67fd129618e662a63238cd0fde071f2f4
Mikhail Alperovich <misha.alperovich1@gmail.com>
2023-12-17 20:34:34 -0500

    Days 16 and 17

---
+++ src/Day16Part1.py
@@ -0,0 +1,104 @@
+import pathlib
+from collections import deque
+from enum import StrEnum, auto
+from typing import List, Deque
+
+CURRENT_PATH = pathlib.Path(__file__)
+IN_FILE = CURRENT_PATH.parent.parent / f'resources/{CURRENT_PATH.stem}_in.txt'
+OUT_FILE = CURRENT_PATH.parent.parent / f'resources/{CURRENT_PATH.stem}_out.txt'
+
+
+class Directions(StrEnum):
+    UP = auto()
+    RIGHT = auto()
+    DOWN = auto()
+    LEFT = auto()
+
+
+class Beam:
+    def __init__(self, i: int, j: int, direction: Directions) -> None:
+        self.i = i
+        self.j = j
+        self.direction = direction
+
+    def move(self):
+        if self.direction == Directions.UP:
+            self.i -= 1
+        elif self.direction == Directions.RIGHT:
+            self.j += 1
+        elif self.direction == Directions.DOWN:
+            self.i += 1
+        elif self.direction == Directions.LEFT:
+            self.j -= 1
+
+    def process_tile(self, grid: List[List[str]]) -> List['Beam']:
+        self.move()
+        if not (0 <= self.i < len(grid) and 0 <= self.j < len(grid[0])):
+            return []
+        tile = grid[self.i][self.j]
+        if tile == '.':
+            return [self]
+        elif tile == '\\':
+            if self.direction == Directions.UP:
+                self.direction = Directions.LEFT
+            elif self.direction == Directions.RIGHT:
+                self.direction = Directions.DOWN
+            elif self.direction == Directions.DOWN:
+                self.direction = Directions.RIGHT
+            elif self.direction == Directions.LEFT:
+                self.direction = Directions.UP
+            return [self]
+        elif tile == '/':
+            if self.direction == Directions.UP:
+                self.direction = Directions.RIGHT
+            elif self.direction == Directions.RIGHT:
+                self.direction = Directions.UP
+            elif self.direction == Directions.DOWN:
+                self.direction = Directions.LEFT
+            elif self.direction == Directions.LEFT:
+                self.direction = Directions.DOWN
+            return [self]
+        elif tile == '-':
+            if self.direction in (Directions.RIGHT, Directions.LEFT):
+                return [self]
+            else:
+                return [Beam(self.i, self.j, Directions.RIGHT), Beam(self.i, self.j, Directions.LEFT)]
+        elif tile == '|':
+            if self.direction in (Directions.UP, Directions.DOWN):
+                return [self]
+            else:
+                return [Beam(self.i, self.j, Directions.UP), Beam(self.i, self.j, Directions.DOWN)]
+        else:
+            raise ValueError(f'Unknown tile type {tile}')
+
+
+def main():
+    lines = IN_FILE.read_text().split('\n')
+    grid = [list(line) for line in lines]
+    visited = set()
+    history = set()
+    queue: Deque[Beam] = deque()
+    queue.append(Beam(0, -1, Directions.RIGHT))
+
+    while len(queue) != 0:
+        b = queue.popleft()
+        for nb in b.process_tile(grid=grid):
+            visited.add((nb.i, nb.j))
+            if (nb.i, nb.j, nb.direction) not in history:
+                history.add((nb.i, nb.j, nb.direction))
+                queue.append(nb)
+
+    # for i in range(len(grid)):
+    #     for j in range(len(grid[0])):
+    #         if (i, j) in visited:
+    #             print('#', end='')
+    #         else:
+    #             print('.', end='')
+    #     print()
+
+    ans = len(visited)
+    OUT_FILE.write_text(str(ans))
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/Day17Part1.py
@@ -0,0 +1,86 @@
+import functools
+import heapq
+import pathlib
+from enum import StrEnum, auto
+from typing import List
+
+CURRENT_PATH = pathlib.Path(__file__)
+IN_FILE = CURRENT_PATH.parent.parent / f'resources/{CURRENT_PATH.stem}_in.txt'
+OUT_FILE = CURRENT_PATH.parent.parent / f'resources/{CURRENT_PATH.stem}_out.txt'
+
+
+class Directions(StrEnum):
+    UP = auto()
+    RIGHT = auto()
+    DOWN = auto()
+    LEFT = auto()
+
+
+MAX_STRAIGHT_LENGTH = 3
+POSSIBLE_DIRECTIONS = {
+    Directions.UP: (Directions.UP, Directions.RIGHT, Directions.LEFT),
+    Directions.RIGHT: (Directions.UP, Directions.RIGHT, Directions.DOWN),
+    Directions.DOWN: (Directions.RIGHT, Directions.DOWN, Directions.LEFT),
+    Directions.LEFT: (Directions.UP, Directions.DOWN, Directions.LEFT)
+}
+MOVE_MATRIX = {
+    Directions.UP: (-1, 0),
+    Directions.RIGHT: (0, 1),
+    Directions.DOWN: (1, 0),
+    Directions.LEFT: (0, -1)
+}
+
+
+@functools.total_ordering
+class Position:
+
+    def __init__(self, graph: List[List[int]], i: int, j: int, direction: Directions, cost: int = 0, smc: int = 0):
+        self._graph = graph
+        self.i = i
+        self.j = j
+        self.direction = direction
+        self.cost = cost
+        self.smc = smc
+
+    def get_next_positions(self) -> List['Position']:
+        positions = []
+        for next_direction in POSSIBLE_DIRECTIONS[self.direction]:
+            move = MOVE_MATRIX[next_direction]
+            next_i, next_j = self.i + move[0], self.j + move[1]
+            next_smc = self.smc + 1 if self.direction == next_direction else 1
+            if not (0 <= next_i < len(self._graph)
+                    and 0 <= next_j < len(self._graph[0])
+                    and next_smc <= MAX_STRAIGHT_LENGTH):
+                continue
+            next_cost = self.cost + self._graph[next_i][next_j]
+            positions.append(Position(self._graph, next_i, next_j, next_direction, next_cost, next_smc))
+        return positions
+
+    def __eq__(self, other):
+        return self.cost == other.cost and self.smc == other.smc
+
+    def __lt__(self, other):
+        if self.cost == other.cost:
+            return self.smc < other.smc
+        return self.cost < other.cost
+
+
+def main():
+    lines = IN_FILE.read_text().split('\n')
+    graph = [[int(t) for t in line] for line in lines]
+    best_cost = {}
+    heap = [Position(graph, 0, 0, Directions.RIGHT)]
+    while len(heap) > 0:
+        pos = heapq.heappop(heap)
+        if pos.i == len(graph) - 1 and pos.j == len(graph[0]) - 1:
+            OUT_FILE.write_text(str(pos.cost))
+            exit()
+        for next_pos in pos.get_next_positions():
+            key = (next_pos.i, next_pos.j, next_pos.direction, next_pos.smc)
+            if key not in best_cost or next_pos.cost < best_cost[key]:
+                best_cost[key] = next_pos.cost
+                heapq.heappush(heap, next_pos)
+
+
+if __name__ == '__main__':
+    main()
